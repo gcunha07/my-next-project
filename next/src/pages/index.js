6e5c1f318027827dca225fafce0ac7d3ea715fa4
gonçalo <goncalon2107@gmail.com>
2025-09-26 16:04:12 +0100

    Componentes

--- next/src/pages/index.js
+++ next/src/pages/index.js
@@ -1,11 +1,26 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import PrimeiroComponente from "./components/PrimeiroComponente"
+import ComponenteComProp from "./components/ComponenteComProp"
+import ComponenteComVariasProps from "./components/ComponenteComVariasProps"
+import ComponenteComCondicional from "./components/ComponenteComCondicional"
+import ComponenteComMap from "./components/ComponenteComMap"
+import ComponenteComEventos from "./components/ComponenteComEventos"
+import ComponenteComState from "./components/ComponenteComState"
+import ComponenteComUseEffect from "./components/ComponenteComUseEffect"
+import ComponenteComFormik from "./components/ComponenteComFormik"
 
-	export default function  Home(){
-		return(
-			<div className="min-h-screen bg-black flex flex-col iems-center justify-center">
-			 <h1 className="text-7xl font-bold text-white">Hello!!</h1>
-			</div>
-
-		)
-	}
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-blue-500 flex flex-col items-center justify-center">
+      <h1 className="text-7xl font-bold text-white">Hello! </h1>
+      <PrimeiroComponente/>
+      <ComponenteComProp/>
+      <ComponenteComVariasProps/>
+      <ComponenteComCondicional/>
+      <ComponenteComMap/>
+      <ComponenteComEventos/>
+      <ComponenteComState/>
+      <ComponenteComUseEffect/>
+      <ComponenteComFormik/>
+    </div>
+  )
+}
