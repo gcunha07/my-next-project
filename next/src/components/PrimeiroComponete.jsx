99c74a5be35db958ef91b443a8122c51993163cc
gonçalo <goncalon2107@gmail.com>
2025-09-26 12:05:39 +0100

    my

---
+++ next/src/components/PrimeiroComponete.jsx
@@ -0,0 +1,9 @@
+export default function
+PrimeiroComponente(){
+	return(
+		<div className="bg-green-10 border border-green-400 text-green-700 px-4 py-3 rounded my-4">
+			<h2 className="text-xl font-semibold">O meu primeiro componente</h2>
+			<p>Este comporente foi criado em src/components/PrimeiroComponete.js</p>
+		</div>
+	)
+}
---
+++ next/src/pages/index.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Geist, Geist_Mono } from "next/font/google";
+
+	export default function  Home(){
+		return(
+			<div className="min-h-screen bg-black flex flex-col iems-center justify-center">
+			 <h1 className="text-7xl font-bold text-white">Hello!!</h1>
+			</div>
+
+		)
+	}
